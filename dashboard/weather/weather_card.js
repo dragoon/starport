c223e3f43dd3a025ddeb541e05d63f066847a4cd
Roman Prokofyev <roman.prokofyev@gmail.com>
2020-12-19 21:43:34 +0100

    remove date

--- dashboard/weather/weather_card.js
+++ dashboard/weather/weather_card.js
@@ -123,7 +123,8 @@ class WeatherCard {
         // d is a Date
         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        this.date.html(days[d.getDay()] + " " + d.getDate() + " " + months[d.getMonth()]);
+        //this.date.html(days[d.getDay()] + " " + d.getDate() + " " + months[d.getMonth()]);
+        this.date.html(days[d.getDay()]);
         TweenMax.fromTo(this.date, 1.5, {x: 30}, {opacity: 1, x: 0, ease: Power4.easeOut});
     }
 
