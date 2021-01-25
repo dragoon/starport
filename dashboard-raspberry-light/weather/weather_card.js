7f6d7a8cd6eb636ec603eee3a3d5bffcef3760f1
Roman Prokofyev <roman.prokofyev@gmail.com>
2021-01-25 16:52:44 +0100

    fix fog position

--- dashboard-raspberry-light/weather/weather_card.js
+++ dashboard-raspberry-light/weather/weather_card.js
@@ -176,6 +176,7 @@ class WeatherCard {
             .quadraticCurveTo(width * -0.5, -arch + height, -width, 0)
             .quadraticCurveTo(-(width * 2), height / 2, -width, height);
         fog.alpha = 0.5;
+        fog.position.y = (this.sizes.card.height - this.settings.cloudHeight - this.settings.cloudSpace * i);
         this.weatherContainers[i].addChild(fog);
         gsap.to(fog, {
             duration: 0,
