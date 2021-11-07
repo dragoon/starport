9d3c2c566ef7bd6fe41ef594d15d18059bdc5ed6
Roman Prokofyev <roman.prokofyev@gmail.com>
2021-11-07 15:29:40 +0100

    simplify update dimensions code

--- dashboard-raspberry-light/sunrise/sunrise.js
+++ dashboard-raspberry-light/sunrise/sunrise.js
@@ -49,19 +49,8 @@ function updateSunrise(brightness) {
 
 
 function updateDimensions() {
-    if (typeof (window.innerWidth) == 'number') {
-        //Non-IE
-        myWidth = window.innerWidth;
-        myHeight = window.innerHeight;
-    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-
-        myWidth = document.documentElement.clientWidth;
-        myHeight = document.documentElement.clientHeight;
-    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
-
-        myWidth = document.body.clientWidth;
-        myHeight = document.body.clientHeight;
-    }
+    myWidth =  window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    myHeight = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
 
 }
 
