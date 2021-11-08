3cacff77c083fc2c685a0a394683ed57ed3d62b3
Roman Prokofyev <roman.prokofyev@gmail.com>
2021-11-08 20:45:56 +0100

    remove all iframes in extras

--- dashboard-raspberry-light/extras/extras.js
+++ dashboard-raspberry-light/extras/extras.js
@@ -1,6 +1,7 @@
 
 
 function addExtras(extras) {
+    document.querySelectorAll("iframe").forEach(e => e.remove());
     extras.forEach(e => {
         document.body.insertAdjacentHTML( 'afterbegin', `<iframe id="${e}" src="extras/${e}"></iframe>` );
     });
