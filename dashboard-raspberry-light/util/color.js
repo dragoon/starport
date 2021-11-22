04affaaefb919fe9c79a754d2899891e843417e2
Roman Prokofyev <roman.prokofyev@gmail.com>
2021-11-22 17:27:56 +0100

    add function fort random normal distribution

--- dashboard-raspberry-light/util/color.js
+++ dashboard-raspberry-light/util/color.js
@@ -149,11 +149,33 @@ function relativeLuminance(colorInt) {
 
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
 function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
+}
+
+
+/**
+ * @return {number|*}
+ */
+function getRandomNormal(min, max, skew=1) {
+    let u = 0, v = 0;
+    while (u === 0) u = Math.random() //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random()
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+
+    num = num / 10.0 + 0.5 // Translate to 0 -> 1
+    if (num > 1 || num < 0)
+        num = getRandomNormal(min, max, skew) // resample between 0 and 1 if out of range
+
+    else {
+        num = Math.pow(num, skew) // Skew
+        num *= max - min // Stretch to fill range
+        num += min // offset to min
+    }
+    return num
 }
