c37c447631ea06f6bb39a31863411e8f66c4a5e9
Roman Prokofyev <roman.prokofyev@gmail.com>
2022-02-05 14:38:28 +0100

    add full calendar instead of date

---
+++ dashboard-raspberry-light/calendar/calendar.js
@@ -0,0 +1,61 @@
+function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+}
+
+function adjustToEuropean(firstDay) {
+    if (firstDay === 0) {
+        return 6;
+    }
+    return firstDay - 1;
+}
+
+function renderCalendar() {
+
+    let today = new Date();
+    let currentMonth = today.getMonth();
+    let currentYear = today.getFullYear();
+    let firstDay = adjustToEuropean((new Date(currentYear, currentMonth)).getDay());
+
+    let calTable = document.getElementById("calendar").lastElementChild; // tbody of the calendar table
+
+    // clearing all previous cells
+    calTable.innerHTML = "";
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth(currentMonth, currentYear)) {
+                break;
+            }
+
+            else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && currentYear === today.getFullYear() && currentMonth === today.getMonth()) {
+                    cell.classList.add("current-day");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+        }
+        calTable.appendChild(row); // appending each row into calendar body.
+    }
+
+}
+
+renderCalendar();
+setTimeout(function () {
+        renderCalendar()
+    }, 60*60000); /* setting timer */
--- dashboard-raspberry-light/datetime/time.js
+++ dashboard-raspberry-light/datetime/time.js
@@ -25,12 +25,9 @@ function currentTime() {
     const date = dateService.getDate();
     let hour = date.getHours();
     let min = date.getMinutes();
-    let sec = date.getSeconds();
     hour = updateTime(hour);
     min = updateTime(min);
-    sec = updateTime(sec);
     document.querySelector(".datetime-container .time").innerHTML = hour + ":" + min;
-    document.querySelector(".datetime-container .date").innerHTML = date.toLocaleDateString("en-US", {weekday: 'long', month: 'long', day: 'numeric'});
     setTimeout(function () {
         currentTime()
     }, 60000); /* setting timer */
